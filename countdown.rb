34ced07093d327bf82d1493ccb8fac0320a8cd24
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-07 23:25:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while number > 0 do
-    number -= 1
+  while number > 1 do
     puts "#{number} SECOND(S)!"
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
